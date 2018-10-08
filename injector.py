23509a5dbf55f9efcddd6b077d3783985bd69254
Martijn de Vos <mdmartijndevos@gmail.com>
2018-10-08 19:01:18 +0200

    Added timeout

--- injector.py
+++ injector.py
@@ -2,13 +2,15 @@ import requests
 import time
 
 while True:
-    time.sleep(10)
+    time.sleep(9)
     print "Creating order..."
     response = requests.put("http://localhost:8085/market/asks", data={
         "first_asset_amount": 10,
         "second_asset_amount": 10,
         "first_asset_type": "DUM1",
-        "second_asset_type": "DUM2"})
+        "second_asset_type": "DUM2",
+        "timeout": 10
+    })
     if response.status_code != 200:
         print "ERROR OCCURED: " % response.text
         exit(1)
